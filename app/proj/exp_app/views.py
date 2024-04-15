6c76f9d3872e080fda631efdb40749f7e552d092
Edmund Walsh <ewalsh@economicdatasciences.com>
2024-04-15 01:10:09 +0100

    layout setup

--- app/proj/exp_app/views.py
+++ app/proj/exp_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from datetime import datetime
+from django.views.generic import TemplateView
+from typing import Any
+
 
 # Create your views here.
+
+# Class Based Views
+class Index(TemplateView):
+    template_name = 'index.html'
+
+    # write a function to pass in current date and time to template
+    async def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        # get current date and time
+        current_date_time = datetime.now()
+        # pass current date and time to template
+        context = {'current_date_time': current_date_time}
+        # render the template
+        return render(request, self.template_name, context)
